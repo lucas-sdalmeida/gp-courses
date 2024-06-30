289281c22081abc7a3a031de0a6d7b002af9b5e9
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2024-06-30 01:38:33 -0300

    chore: project creation and initial structuring

---
+++ gp-courses-web/src/main/java/com/lucassdalmeida/gatopianista/gpcourses/web/GpCoursesWebApplication.java
@@ -0,0 +1,13 @@
+package com.lucassdalmeida.gatopianista.gpcourses.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpCoursesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpCoursesWebApplication.class, args);
+	}
+
+}
